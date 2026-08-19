22a4008e14ae6c202ff36dca01a804b4062ea99e
qiqd <129611515+qiqd@users.noreply.github.com>
2026-08-19 22:38:23 +0800

    feat(env): 更新环境变量配置，添加默认值并移除可选字段支持 feat(hive): 添加Hive适配器注册扩展以支持实体注册

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -2,18 +2,30 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-@Envied(path: '.env', obfuscate: true, allowOptionalFields: true)
+@Envied(path: '.env', obfuscate: true)
 abstract class Env {
   static const String defaultMetaServerUrl = "https://api.bgm.tv";
-  static const String defaultImgServerHost = "https://lain.bgm.tv";
+  static const String defaultImgServerUrl = "https://lain.bgm.tv";
+  static const String defaultDammakuServerUrl = "";
 
-  @EnviedField(varName: 'DAMMAKU_SERVER_URL')
-  static final String dammakuServerUrl = _Env.dammakuServerUrl ?? '';
+  @EnviedField(
+    varName: 'DAMMAKU_SERVER_URL',
+    obfuscate: true,
+    defaultValue: defaultDammakuServerUrl,
+  )
+  static final String dammakuServerUrl = _Env.dammakuServerUrl;
 
-  @EnviedField(varName: 'META_SERVER_URL')
-  static final String metaServerUrl =
-      _Env.metaServerUrl ?? defaultMetaServerUrl;
+  @EnviedField(
+    varName: 'META_SERVER_URL',
+    obfuscate: true,
+    defaultValue: defaultMetaServerUrl,
+  )
+  static final String metaServerUrl = _Env.metaServerUrl;
 
-  @EnviedField(varName: 'IMG_SERVER_URL')
-  static final String imgServerUrl = _Env.imgServerUrl ?? defaultImgServerHost;
+  @EnviedField(
+    varName: 'IMG_SERVER_URL',
+    obfuscate: true,
+    defaultValue: defaultImgServerUrl,
+  )
+  static final String imgServerUrl = _Env.imgServerUrl;
 }
